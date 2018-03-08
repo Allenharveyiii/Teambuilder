6fd361d29477cb5fd2e849a89066e113115ec5c0
Nathan Greene <nathangreene3@gmail.com>
2018-03-08 18:54:27 -0500

    Added comments and further clarification.  Improved driver.  Removed display options used in debugging.  Still need to implement size constraint.

--- app/Http/Controllers/kmeans.php
+++ app/Http/Controllers/kmeans.php
@@ -1,54 +1,55 @@
 <?php
-/**
+/*******************************************************************************
  * kmeans.php
+ *******************************************************************************
  * Nathan Greene
  * Spring 2018
  *
- * This file contains the kmeans class, which accepts an integer k as the number
- * of clusters and a list of data points.
+ * This file contains the kmeans class, which accepts an integer k as
+ * the number of clusters and a list of data points.
  */
 
- /**
-  * kmeans
-  * Clusters similar data types into the nearest cluster of k available clusters.
+ /******************************************************************************
+  * kmeans class
+  ******************************************************************************
+  * Clusters similar data types into the nearest cluster of k available
+  * clusters.
   */
 class kmeans
 {
-	private $data;		    // Array of multidimensional points to assign to clusters; data = [ [x0, x1, ...], [x0, x1, ...], ... ]
+	private $data;		    // Array of multidimensional points to assign to clusters; data = [[x0, x1, ...], [x0, x1, ...], ...]
     private $data_length;   // Number of data points in data
     private $dims;          // Number of dimensions in each data point
     private $k;			    // Number of clusters to assign data items to
-	private $means;	    	// Array of k points representing the mean of data points assigned to each indexed mean; means = [ [x0, x1, ...], [x0, x1, ...], ... ]
-    private $clusters;      // Array of integers indicating which cluster each point is assigned to; data[i] is assigned to cluster indexed by clusters[i]
+	private $means;	    	// Array of k points representing the mean of data points assigned to each indexed mean; means = [[x0, x1, ...], [x0, x1, ...], ...]
+    private $clusters;      // Array of integers indicating which cluster each point is assigned to; data[i] = [[c0, vi0], [c1, vi1], ...] is a priority queue of clusters with variances
 	private $cluster_sizes; // Array of integers indicating how many data points are assigned to each cluster
 
-	/**
-	 * kmeans(int,[double[]])
-	 * Constructs the kmeans object given k clusters and an array of
-	 * double-valued points as the data to cluster. Means are initialized as
-	 * random data points on the range [0, 1]. For example, cluster0 might be
-	 * defined as
-	 * 		means[0] = [ [0.1, 0.2, 0.0], ..., [0.7, 0.3, 0.5] ]
-	 * for three-dimensional data points.
-	 */
+    /***************************************************************************
+     * kmeans (int, [float[]])
+     ***************************************************************************
+     * Constructs the kmeans object given k clusters and an array of numeric
+	 * points as the data to cluster. Means are initialized as random data
+	 * points on the range [0, 1].
+     * @param int $k
+     * @param array $data
+     */
 	public function __construct(int $k, array $data)
 	{
-		/// k must be positive, all data dimensions must be equal
 		if (0 < $k && $this->check_dims($data))
-		{
 			$this->initialize($k, $data);
-			//$this->update_clusters();
-		}
 		else
-		    $this->die("kmeans.php error: Invalid choice of k or data is inconsistently defined.");
+		    die ("kmeans.php error: Invalid choice of k or data is inconsistently defined.");
 	}
 
-	/**
-	 * check_dims([double[]]):bool
-	 * Determine if all sublists are of the same length. This function is a
-	 * stand-alone function that is independent of the properties that define
-	 * this kmeans class.
-	 */
+	/***************************************************************************
+	 * check_dims ([float[]]):bool
+     ***************************************************************************
+	 * Returns true if all data points are of the same length and false if
+	 * otherwise.
+     * @param array $data
+     * @return bool $result
+     */
 	private function check_dims(array $data)
 	{
 		$data_length = sizeof($data);
@@ -60,8 +61,13 @@ class kmeans
 		return $result;
 	}
 
-	/**
-	 * initialize_means(int, [double[]]):void
+	/***************************************************************************
+	 * initialize_means(int, [float[]]):void
+	 ***************************************************************************
+	 * Initializes all properties of the kmeans class. Assumes k > 0 and data
+	 * dimensions are consistent. Further assumes data is all numeric.
+	 * @param int $k
+	 * @param array $data
 	 */
     private function initialize(int $k, array $data)
     {
@@ -93,18 +99,24 @@ class kmeans
             $this->cluster_sizes[$i] = 0;
     }
 
-	/**
-	 * get_means():[double[]]
+	/***************************************************************************
+	 * get_means ():[float[]]
+	 ***************************************************************************
 	 * Retrieve the means attribute.
+	 * @return array $this->means
 	 */
 	public function get_means()
     {
         return $this->means;
     }
 
-	/**
-	 * variance(int, int):double
+	/***************************************************************************
+	 * variance (int, int):float
+	 ***************************************************************************
 	 * Determine the variance of a point to a cluster (mean).
+	 * @param int $cluster_index
+	 * @param int $point_index
+	 * @return float $value
 	 */
     public function variance(int $cluster_index, int $point_index)
     {
@@ -115,22 +127,31 @@ class kmeans
             	$value += pow($this->data[$point_index][$i] - $this->means[$cluster_index][$i], 2);
 		}
 		else
-			throw new ArgumentOutOfRangeException("kmeans.php error: cluster index must be on range [0, k - 1] and point index must be on range [0, data length - 1]");
+			throw new Exception("kmeans.php error: cluster index must be on range [0, k - 1] and point index must be on range [0, data length - 1]");
         return $value;
     }
 
-	/**
-	 * distance(int, int):double
+	/***************************************************************************
+	 * distance (int, int):float
+     ***************************************************************************
 	 * Determine the Euclidean distance from a data point to a given cluster
 	 * (mean).
+	 * @param int $cluster_index
+	 * @param int $point_index
+	 * @return float
 	 */
     public function distance(int $cluster_index, int $point_index)
     {
         return sqrt($this->variance($cluster_index, $point_index));
     }
 
-	/**
-	 * TODO
+	/***************************************************************************
+	 * update_means():bool
+	 ***************************************************************************
+     * Update all means as the average value of the data points assigned to each
+	 * mean. If no change occurs in the means, returns true. Otherwise, returns
+	 * false.
+	 * @return bool
 	 */
     private function update_means()
     {
@@ -142,11 +163,14 @@ class kmeans
             for ($j = 0; $j < $this->dims; $j++)
                 $this->means[$i][$j] = 0;
 
+		/// Update means as the sum of all assigned data points. Data points are
+		/// assigned as the index of the first cluster in the cluster queue.
         for ($i = 0; $i < $this->data_length; $i++)
             for ($j = 0; $j < $this->dims; $j++)
                 $this->means[$this->clusters[$i][0][0]][$j] += $this->data[$i][$j];  // Add each data point to mean that it is assigned to
 
-        /// Iterate through each mean dividing by size. If size is zero, redefine mean as random point.
+        /// Iterate through each mean dividing by size. If size is zero,
+		/// redefine mean i as a random point.
         for ($i = 0; $i < $this->k; $i++)
             if ($this->cluster_sizes[$i] == 0)
                 for ($j = 0; $j < $this->dims; $j++)
@@ -157,8 +181,9 @@ class kmeans
         return $old_means == $this->means ? true : false;   // Return true if no change occurs
     }
 
-	/**
+	/***************************************************************************
 	 * update_clusters():void
+	 ***************************************************************************
 	 * Update cluster assignments by searching through variances property. The
 	 * column index (cluster mean) having the smallest variance is set for each
 	 * data point index in the cluster assignment property. That is, the cluster
@@ -167,130 +192,118 @@ class kmeans
 	 */
 	private function update_clusters()
 	{
-        print ("\n\nClusters BEFORE sorting");
-        for ($i = 0; $i < $this->k; $i++)
-            $this->display_cluster($i);
-
         /// Reset cluster sizes to zero
         for ($i = 0; $i < $this->k; $i++)
             $this->cluster_sizes[$i] = 0;
 
+        /// Compute the variance for each cluster j in the queue for each data point i
+        /// Sort the cluster queue i based on variance
+        /// Update the size of the cluster data point i is assigned to (having the smallest variance)
 		for ($i = 0; $i < $this->data_length; $i++)
         {
             for ($j = 0; $j < $this->k; $j++)
                 $this->clusters[$i][$j] = [$j, $this->variance($j, $i)];    // Set variance for each cluster j to data point i
-            $this->sort_cluster($i, false);							// True sorts on cluster id; False sorts on variance
+            $this->sort_cluster($i, 1);										// 0 sorts on cluster id; 1 sorts on variance
             $this->cluster_sizes[$this->clusters[$i][0][0]]++;              // Increment the size of cluster in which data point i is now assigned to
         }
-
-//        print ("\n\nClusters AFTER sorting");
-//        for ($i = 0; $i < $this->k; $i++)
-//            $this->display_cluster($i);
 	}
 
-    /**
-     * @param array $cluster_pq
-     * @param bool $cluster_id
-     *
-     * Just cocktailsort for now. TODO: Quicksort.
+    /***************************************************************************
+	 * sort_cluster(int, int):void
+	 ***************************************************************************
+	 * Sorts the cluster queue of the specified data point on the specified
+	 * column. If zero is passed, then the cluster queue is sorted on the
+	 * cluster index. If one is passed, then the cluster queue is sorted on the
+	 * variance of the specified point to the clusters. This sorting method is
+	 * commonly called cocktail sorting and is analagous to bubble sorting in
+	 * both directions.
+     * @param array $point_index
+     * @param int $column
      */
-	private function sort_cluster(int $point_index, bool $cluster_id)
+	private function sort_cluster(int $point_index, int $column)
     {
-        if ($cluster_id)
-            $col = 0;   // sort on cluster id
-        else
-            $col = 1;   // sort on variance
-        $changed = true;
-        $start   = 0;
-        $end     = $this->k - 1;
-        while ($changed)
+        if ($column == 0 || $column == 1)
         {
-            $changed = false;
-            for ($i = $start; $i < $end; $i++)
-            {
-                if ($this->clusters[$point_index][$i + 1][$col] < $this->clusters[$point_index][$i][$col])
-                {
-                    $temp = $this->clusters[$point_index][$i];
-                    $this->clusters[$point_index][$i]     = $this->clusters[$point_index][$i + 1];
-                    $this->clusters[$point_index][$i + 1] = $temp;
-                    $changed = true;
-                }
-            }
-            $end--;
-            for ($i = $end; $start < $i; $i--)
-            {
-                if ($this->clusters[$point_index][$i][$col] < $this->clusters[$point_index][$i - 1][$col])
-                {
-                    $temp = $this->clusters[$point_index][$i];
-                    $this->clusters[$point_index][$i]     = $this->clusters[$point_index][$i - 1];
-                    $this->clusters[$point_index][$i - 1] = $temp;
-                    $changed = true;
-                }
-            }
-            $start++;
-        }
-		/*for ($i = 1; $i < $this->k; $i++)
-		{
-			if ($this->clusters[$point_index][$i][$col] < $this->clusters[$point_index][0][$col])
-			{
-				$temp = $this->clusters[$point_index][0];
-				$this->clusters[$point_index][0]  = $this->clusters[$point_index][$i];
-				$this->clusters[$point_index][$i] = $temp;
-			}
-		}*/
+			$changed = true;			// Indicates if change occurs in sorting
+	        $start   = 0;				// Left index to iterate from
+	        $end     = $this->k - 1;	// Right index to iterate to
+	        while ($changed)
+	        {
+	            $changed = false;
+
+				/// Bubblesort forward pushing largest value right
+	            for ($i = $start; $i < $end; $i++)
+	            {
+	                if ($this->clusters[$point_index][$i + 1][$column] < $this->clusters[$point_index][$i][$column])
+	                {
+	                    $temp = $this->clusters[$point_index][$i];
+	                    $this->clusters[$point_index][$i]     = $this->clusters[$point_index][$i + 1];
+	                    $this->clusters[$point_index][$i + 1] = $temp;
+	                    $changed = true;
+	                }
+	            }
+	            $end--;
+
+				/// Bubblesort backward pushing smallest value left
+	            for ($i = $end; $start < $i; $i--)
+	            {
+	                if ($this->clusters[$point_index][$i][$column] < $this->clusters[$point_index][$i - 1][$column])
+	                {
+	                    $temp = $this->clusters[$point_index][$i];
+	                    $this->clusters[$point_index][$i]     = $this->clusters[$point_index][$i - 1];
+	                    $this->clusters[$point_index][$i - 1] = $temp;
+	                    $changed = true;
+	                }
+	            }
+	            $start++;
+	        }
+		}
     }
 
+	/***************************************************************************
+	 * run():[int]
+	 ***************************************************************************
+	 * Performs kmeans algorithm by repeatedly updating means until no change
+	 * occurs (i.e. means converge). Returns the cluster assignments for the
+	 * data points in their original positions. That is, cluster i = j means
+	 * data point i is in the cluster identified as j.
+	 * @return array $clusters
+	 */
     public function run()
     {
-        $notchanged = false;
-        for ($i = 0; $i < 25 && !$notchanged; $i++)
+        $iterations  = 0;		// Keeps kmeans from running indefinitely
+        $not_changed = false;	// Indicates if change occurs (convergence)
+        while (!$not_changed && $iterations < 1000)
         {
-            print("\nStep ".$i);
-            $notchanged = $this->update_means();
+            $not_changed = $this->update_means();
             $this->update_clusters();
-            if ($notchanged)
-                print ("\nIt converged.");
+            $iterations++;
         }
-    }
-
-    public function display_cluster(int $cluster_index)
-    {
-        print ("\n\ncluster ".$cluster_index." = ".$this->display_mean($cluster_index)." of size ".$this->cluster_sizes[$cluster_index]);
+        $clusters = array();
         for ($i = 0; $i < $this->data_length; $i++)
-            if ($this->clusters[$i][0][0] == $cluster_index)
-                print ("\npoint ".$i." = ".$this->display_point($i)." has var = ".number_format($this->variance($cluster_index, $i),2)." and belongs to cluster ".$this->clusters[$i][0][0]);    //$this->clusters[$i][0][1]
-    }
-
-    public function display_mean(int $mean_index)
-    {
-        $value = "[";
-        for ($i = 0; $i < $this->dims; $i++)
-            $value .= " ".number_format($this->means[$mean_index][$i], 2);
-        $value .= "]";
-        return $value;
-    }
-
-    public function display_point(int $point_index)
-    {
-        $value = "[";
-        for ($i = 0; $i < $this->dims; $i++)
-            $value .= " ".number_format($this->data[$point_index][$i],2);
-        $value .= "]";
-        return $value;
+            $clusters[$i] = $this->clusters[$i][0][0];
+        return $clusters;
     }
 }
 
 /// Driver
-$kmeans = new kmeans(2, array([0.1, 0.2], [0.3, 0.4], [0.5, 0.6], [0.7, 0.8]));
-$means  = $kmeans->get_means();
-$means  = $kmeans->run();
-$i = 0;
-print("\t\t   x       y\n");
-foreach ($means as $cluster)
+$k = 5;
+$data_length = 1000;
+$dims = 3;
+//$data = array([3], [5], [9], [8], [2], [3], [7], [8], [4], [9]);
+$data = array();
+for ($i = 0; $i < $data_length; $i++)
 {
-	print("Cluster " . $i . ":\t");
-	foreach ($cluster as $value)
-		print(number_format($value, 2) . "\t"); // or round(,)
-	print("\n");
-	$i++;
+	$data[$i] = array();
+	for ($j = 0; $j < $dims; $j++)
+		$data[$i][$j] = rand(0, getrandmax()) / getrandmax();
+}
+$kmeans   = new kmeans($k, $data);
+$clusters = $kmeans->run();
+for ($i = 0; $i < sizeof($data); $i++)
+{
+	print ("\nData point ".$i." = [".number_format($data[$i][0] ,1));
+	for ($j = 1; $j < $dims; $j++)
+		print(" ".number_format($data[$i][$j], 1));
+	print("] is in cluster ".$clusters[$i]);
 }
